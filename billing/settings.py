c2e32ce0e26ec72a96e240b5c09b9b886d1c8da8
encypted10 <kmarsalan.com>
2025-09-21 18:04:12 +0530

    postgresql

--- billing/settings.py
+++ billing/settings.py
@@ -77,12 +77,38 @@ WSGI_APPLICATION = 'billing.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'starfashion',
+#         'USER': 'khajamubashirarsalan',
+#         'PASSWORD': '9Ul4eLMYZNuERW19WRliYk8HTdFDzhAL',
+#         'HOST': 'dpg-d37tu9ruibrs739bmohg-a.oregon-postgres.render.com',
+#         'PORT': '5432',
+#     }
+# }
+
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT', 5432),
     }
 }
+
+
 
 
 # Password validation
